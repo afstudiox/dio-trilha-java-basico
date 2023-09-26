2eb50030c8b0c180f5437f81c1c4aec50fa163a4
Andre Felipe <afelipes@gmail.com>
2023-09-26 15:07:04 -0300

    Finish Learn Types & Variables

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Andre Felipe";
+        
+        double salarioMinimo = 1100.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double PI = 3.14;
+        System.out.println(PI);
     }
 }
