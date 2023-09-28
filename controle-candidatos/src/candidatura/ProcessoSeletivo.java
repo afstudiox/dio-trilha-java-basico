6bd2e593a07bc1df3eeb496c7a133ab5dceb6919
Andre Felipe <afelipes@gmail.com>
2023-09-28 00:30:51 -0300

    refatoração dos métodos da aulas - selecionarCandidatos() recebe um array de candidatos e retorna os selecionados - entrarEmContato() melhorou a lógica de verificar atendimento - main faz uma filtragem se o array de selecionados foi todo preenchidos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -4,25 +4,27 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
+        String [] candidatos = 
+            {"João", "Maria", "José", "Ana", "Pedro", "Paulo", "Lucas", "Marcos", "Mateus", "Tiago", "Julia", "Patricia", "Carla" }; 
         System.out.println("Processo Seletivo");
-        String [] candidatos = {"João", "Maria", "José", "Ana", "Pedro"};
-        for (String candidato : candidatos) {
-            entrarEmContato(candidato);
+        String [] candidatosSelecionados = selecionarCandidatos(candidatos);
+        for (String candidato : candidatosSelecionados) {
+            if (candidato != null){
+                entrarEmContato(candidato);
+            }
         }
     }
 
     static void entrarEmContato(String candidato){
         int tentativasRealizadas = 1;
-        boolean continuarTentando = true;
         boolean atendeu = false;
         do {
             atendeu = atender();
-            continuarTentando = !atendeu;
-            if (continuarTentando) {
+            if (!atendeu) {
                 tentativasRealizadas++;
             } 
 
-        } while (continuarTentando && tentativasRealizadas < 3);
+        } while (!atendeu && tentativasRealizadas < 3);
 
         if (atendeu){
             System.out.println("O candidato " + candidato + " atendeu o telefone na tentativa " + tentativasRealizadas);
@@ -32,52 +34,29 @@ public class ProcessoSeletivo {
 
     }
 
+    static String[] selecionarCandidatos(String[] candidatos) {
+        String[] candidatosSelecionados = new String[5];
+        double salarioBase = 2000.00;
+        int selecionados = 0;
+        for ( int i = 0; i < candidatos.length; i++){
+            double salarioPretendido = valorPretendido();
+            if (salarioBase >= salarioPretendido && selecionados < 5) {
+                candidatosSelecionados[selecionados]=candidatos[i];
+                selecionados++;
+            } 
+        }
+        return candidatosSelecionados;
+    }
+
+
+    // Métodos randômicos para simulação
+
     static boolean atender(){
         return new Random().nextInt(3) == 1;
-    }
-
-
-    static void imprimirSelecioandos() {
-        String [] candidatos = 
-            {"João", "Maria", "José", "Ana", "Pedro"}; 
-        System.out.println("Imprimindo a lista de candadidatos");
-        for (int indice = 0; indice < candidatos.length; indice++) {
-            System.out.println("Número do candidato: " + indice + 1 + " Candidato: " + candidatos[indice]);
-        }
-    }
-
-    static void selecionarCandidatos() {
-        String [] candidatos = 
-            {"João", "Maria", "José", "Ana", "Pedro", "Paulo", "Lucas", "Marcos", "Mateus", "Tiago", "Julia", "Patricia", "Carla" }; 
-        int candidatosSelecionados = 0;
-        int candidatoAtual = 0;
-        double salarioBase = 2000.00;
-        while ( candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
-            String candidato = candidatos[candidatoAtual];
-            double salarioPretendido = valorPretendido();
-
-            System.out.println("O candidato " + candidato + " solictou este valor salarial -> " + salarioPretendido);
-            if (salarioBase >= salarioPretendido){
-                System.out.println("O candidato " + candidato + " foi selecionado");
-                candidatosSelecionados++;
-            } 
-            candidatoAtual++;
-        }
     }
 
     static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800.00, 2200.00);
     }
 
-    static void analisarCandidato(double SalarioPretendido) {
-        double salarioBase = 2000.00;
-        if (salarioBase > SalarioPretendido){
-            System.out.println("Ligar para o candidato");
-        } else if (salarioBase == SalarioPretendido) {
-            System.out.println("Ligar para o candidato com contra proposta");
-        } else {
-            System.out.println("Aguardando o resultado dos demais candidatos");
-        }
-
-    }
 }
