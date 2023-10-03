b71558d94f7363c3695197d76c5bbc2ae5f4ea1f
Andre Felipe <afelipes@gmail.com>
2023-10-03 14:39:10 -0300

    DesafioControleFLuxo  concluído com sucesso

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.print("Digite o primeiro parametro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.print("Digite o segundo parametro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroDois <= parametroUm) {
+            throw new ParametrosInvalidosException();
+        }
+
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
