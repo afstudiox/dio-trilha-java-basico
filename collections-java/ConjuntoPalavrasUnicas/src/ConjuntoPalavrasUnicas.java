067e195beee5be1886d2ca4a2a0b996e423c57a3
Andre Felipe <afelipes@gmail.com>
2023-10-05 17:08:54 -0300

    Desafio Set - Conjunto de Palavras ùnicas

---
+++ collections-java/ConjuntoPalavrasUnicas/src/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoPalavrasUnicas {
+    // atributos
+    private Set<String> palavrasUnicasSet;
+
+    public ConjuntoPalavrasUnicas() {
+        this.palavrasUnicasSet = new HashSet<>();
+    }
+
+    public void adicionarPalavra(String palavra) {
+        this.palavrasUnicasSet.add(palavra);
+    }
+
+    public void removerPalavra(String palavra) {
+        this.palavrasUnicasSet.remove(palavra);
+        System.out.println("A palavra " + palavra + " foi removida do conjunto.");
+    }
+
+    public void verificarPalavra(String palavra) {
+        if (this.palavrasUnicasSet.contains(palavra)) {
+            System.out.println("A palavra " + palavra + " está no conjunto.");
+        } else {
+            System.out.println("A palavra " + palavra + " não está no conjunto.");
+        }
+    }
+
+    public void exibirPalavrasUnicas() {
+        System.out.println("Palavras únicas: " + this.palavrasUnicasSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas conjuntoPalavrasUnicas = new ConjuntoPalavrasUnicas();
+
+        conjuntoPalavrasUnicas.adicionarPalavra("casa");
+        conjuntoPalavrasUnicas.adicionarPalavra("carro");
+        conjuntoPalavrasUnicas.adicionarPalavra("chapeu");
+        conjuntoPalavrasUnicas.adicionarPalavra("sapato");
+
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        conjuntoPalavrasUnicas.verificarPalavra("casa");
+
+        conjuntoPalavrasUnicas.removerPalavra("casa");
+
+        conjuntoPalavrasUnicas.verificarPalavra("casa");
+
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+    }
+
+}
