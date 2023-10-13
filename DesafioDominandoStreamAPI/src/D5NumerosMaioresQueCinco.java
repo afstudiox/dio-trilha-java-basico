ff8b48df53b8a0daf192253a3f34be1d1f734b2e
Andre Felipe <afelipes@gmail.com>
2023-10-13 14:43:57 -0300

    Desafio 5 - Calcule a média dos números maiores que 5

---
+++ DesafioDominandoStreamAPI/src/D5NumerosMaioresQueCinco.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class D5NumerosMaioresQueCinco {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        OptionalDouble media = numeros.stream()
+        .filter(numero -> numero > 5)
+        .mapToDouble(numero -> numero)
+        .average();
+
+        if (media.isPresent()) {
+            System.out.println(media.getAsDouble());
+        } else {
+            System.out.println("Não há média");
+        }
+    }
+}
